1dea1eabe9b0f73243096f23d2aa92c03596f59e
Sz-Tim <tim.szewczyk@gmail.com>
2024-06-11 08:37:19 +0100

    Clarifying needLight requirements

--- biotracker/RunProperties.java
+++ biotracker/RunProperties.java
@@ -237,7 +237,7 @@ public class RunProperties {
         needT = viableDegreeDays > -1;
         needZeta = false;
         needK = !fixDepth && variableDhV;
-        needLight = !fixDepth;
+        needLight = !fixDepth && swimLightLevel;
         needVh = variableDh;
 
         properties.list(System.out);
